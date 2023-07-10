af8aa0c72a6089cc062a0ebc1e58d8ff63b414cc
Christian Gabbard <christiangabbard@gmail.com>
2023-07-10 14:26:13 -0700

    updated duplicate seeders

--- backend/db/migrations/20230706220413-create-user.js
+++ backend/db/migrations/20230706220413-create-user.js
@@ -1,47 +1,76 @@
 "use strict";
 
-let options = {};
-if (process.env.NODE_ENV === 'production') {
-  options.schema = process.env.SCHEMA;  // define your schema in options object
-}
-
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return queryInterface.createTable("Users", {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      username: {
-        type: Sequelize.STRING(30),
-        allowNull: false,
-        unique: true
-      },
-      email: {
-        type: Sequelize.STRING(256),
-        allowNull: false,
-        unique: true
-      },
-      hashedPassword: {
-        type: Sequelize.STRING.BINARY,
-        allowNull: false
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-      }
-    }, options);
+    await queryInterface.addColumn("Users", "firstName", {
+      type: Sequelize.STRING(30),
+      allowNull: true,
+    });
+
+    await queryInterface.addColumn("Users", "lastName", {
+      type: Sequelize.STRING(30),
+      allowNull: true,
+    });
+
+    // Add other existing columns as necessary
+    await queryInterface.changeColumn("Users", "username", {
+      type: Sequelize.STRING(30),
+      allowNull: false,
+      unique: true,
+    });
+
+    await queryInterface.changeColumn("Users", "email", {
+      type: Sequelize.STRING(256),
+      allowNull: false,
+      unique: true,
+    });
+
+    await queryInterface.changeColumn("Users", "hashedPassword", {
+      type: Sequelize.STRING.BINARY,
+      allowNull: false,
+    });
+
+    await queryInterface.changeColumn("Users", "createdAt", {
+      allowNull: false,
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+    });
+
+    await queryInterface.changeColumn("Users", "updatedAt", {
+      allowNull: false,
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+    });
   },
+
   down: async (queryInterface, Sequelize) => {
-    options.tableName = "Users";
-    return queryInterface.dropTable(options);
-  }
+    await queryInterface.removeColumn("Users", "firstName");
+    await queryInterface.removeColumn("Users", "lastName");
+
+    // Rollback changes for other existing columns if necessary
+    await queryInterface.changeColumn("Users", "username", {
+      type: Sequelize.STRING(30),
+      allowNull: false,
+    });
+
+    await queryInterface.changeColumn("Users", "email", {
+      type: Sequelize.STRING(256),
+      allowNull: false,
+    });
+
+    await queryInterface.changeColumn("Users", "hashedPassword", {
+      type: Sequelize.STRING.BINARY,
+      allowNull: false,
+    });
+
+    await queryInterface.changeColumn("Users", "createdAt", {
+      allowNull: false,
+      type: Sequelize.DATE,
+    });
+
+    await queryInterface.changeColumn("Users", "updatedAt", {
+      allowNull: false,
+      type: Sequelize.DATE,
+    });
+  },
 };
--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,52 +1,55 @@
-'use strict';
-const { Model, Validator } = require('sequelize');
+"use strict";
+const { Model, DataTypes } = require("sequelize");
+const { sequelize } = require("../../db/models");
 
-module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    static associate(models) {
-      // define association here
-    }
-  };
+class User extends Model {
+  static associate(models) {
+    // define association here
+  }
+}
 
-  User.init(
-    {
-      username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [4, 30],
-          isNotEmail(value) {
-            if (Validator.isEmail(value)) {
-              throw new Error("Cannot be an email.");
-            }
-          }
-        }
-      },
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [3, 256],
-          isEmail: true
-        }
-      },
-      hashedPassword: {
-        type: DataTypes.STRING.BINARY,
-        allowNull: false,
-        validate: {
-          len: [60, 60]
-        }
-      }
+User.init(
+  {
+    firstName: {
+      type: DataTypes.STRING(30),
+      allowNull: true,
     },
-    {
-      sequelize,
-      modelName: "User",
-      defaultScope: {
-        attributes: {
-          exclude: ["hashedPassword", "email", "createdAt", "updatedAt"]
-        }
-      }
-    }
-  );
-  return User;
-};
+    lastName: {
+      type: DataTypes.STRING(30),
+      allowNull: true,
+    },
+    username: {
+      type: DataTypes.STRING(30),
+      allowNull: false,
+      validate: {
+        len: [4, 30],
+      },
+    },
+    email: {
+      type: DataTypes.STRING(256),
+      allowNull: false,
+      validate: {
+        len: [3, 256],
+        isEmail: true,
+      },
+    },
+    hashedPassword: {
+      type: DataTypes.STRING.BINARY,
+      allowNull: false,
+      validate: {
+        len: [60, 60],
+      },
+    },
+  },
+  {
+    sequelize,
+    modelName: "User",
+    defaultScope: {
+      attributes: {
+        exclude: ["hashedPassword", "email", "createdAt", "updatedAt"],
+      },
+    },
+  }
+);
+
+module.exports = User;
